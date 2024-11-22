90eb088230b0765136e1a3ee41322f24694f2641
BillyEduardo <billidionicion@gmail.com>
2024-11-22 09:55:26 -0400

    Funcion de resta

--- Calculadora/Menu.cs
+++ Calculadora/Menu.cs
@@ -18,11 +18,11 @@ namespace Calculadora
 		{
 			switch (opcion)
 			{
-				case 1:
-					Console.WriteLine("Has seleccionado Sumar.");
+				case 1: // Sumar
+					Console.WriteLine("Has seleccionado Suma");  // Llamamos al método de la clase Suma
 					break;
-				case 2:
-					Console.WriteLine("Has seleccionado Restar.");
+				case 2: // Restar
+					Resta.RealizarResta(); // Llamamos al método de la clase Resta
 					break;
 				case 3:
 					Console.WriteLine("Has seleccionado Multiplicar.");
